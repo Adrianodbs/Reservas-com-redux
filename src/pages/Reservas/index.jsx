04b788fdf48f928d1f50cf1930d710157cd80751
Adriano <adriano_alves_2008@hotmail.com>
2023-05-03 14:44:08 -0300

    Estilizando Reservas

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import './style.css'
+import { MdDelete } from 'react-icons/md'
 
 function Reservas() {
-  return <div>Reservas</div>
+  return (
+    <div>
+      <h1 className="title">Você solicitou 1 reserva</h1>
+
+      <div className="reservas">
+        <img src="" alt="" />
+        <strong>Viagem maceio</strong>
+        <span>Quatidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+
+      <footer>
+        <button type="button">Solicitar reservas</button>
+      </footer>
+    </div>
+  )
 }
 
 export default Reservas
