ea5b3d8628b5418f1281869848f05190c6085252
Adriano <adriano_alves_2008@hotmail.com>
2023-05-03 14:31:32 -0300

    configurando api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import api from '../../services/api'
 
 function Home() {
+  const [trips, setTrips] = useState([])
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips')
+
+      setTrips(response.data)
+
+      console.log(response.data)
+    }
+
+    loadApi()
+  }, [])
   return <div>Home</div>
 }
 
